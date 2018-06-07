623d1f4238d89dfda63bbfc702ae44e08cb59e69
Alysa Barbarisi <baal02@cox.net>
2018-06-07 21:40:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
 # person.rb
+class Person 
+  
+  attr_accessor :adele_goldberg, :alan_kay 
+  
+  def initialize(adele_goldberg, alan_kay)
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end 
+end 
